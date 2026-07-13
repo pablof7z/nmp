f01177b38c2266b01eec451289cc489d5401af1f
pablof7z <pfer@me.com>
2026-07-13 11:17:58 +0300

    Restore rustfmt-clean master

--- crates/nmp-engine/src/core/mod.rs
+++ crates/nmp-engine/src/core/mod.rs
@@ -47,8 +47,8 @@ use nmp_grammar::{
 };
 use nmp_resolver::{Engine as ResolverEngine, HandleId, LiveQuery, QueryHandle};
 use nmp_router::{
-    DiscoveryKinds, Lane, LanedRelay, PubkeyHex, RelayDirectory, Router, RuleRegistry,
-    SubId, WireDelta, WireOp, WireReq,
+    DiscoveryKinds, Lane, LanedRelay, PubkeyHex, RelayDirectory, Router, RuleRegistry, SubId,
+    WireDelta, WireOp, WireReq,
 };
 use nmp_signer::SignerError;
 use nmp_store::{
